300840804cd50b3218a093e3e3a1055de101caf1
Carlyne Lapierre <carlyne.lapierre@gmail.com>
2020-05-18 14:31:49 +0200

    sidebar with base needed style and working links

--- src/components/app-sidebar/AppSidebar.vue
+++ src/components/app-sidebar/AppSidebar.vue
@@ -1,6 +1,17 @@
 <template>
-    <header id="app-sidebar" class="navbar col sm-w fixed top left">
-        sidebar
+    <header id="app-sidebar" class="navbar col sm-w fixed top left d-around-s">
+        <router-link to="/"><img class="col-logo" src="../../assets/logo-krly.png" alt="logo"></router-link>
+        <nav>
+            <ul class="navbar-menu">
+                <li>Projets</li>
+                <li>Experimentations</li>
+                <li>Illustrations</li>
+                <li><router-link to="/about">A propos</router-link></li>
+            </ul>
+        </nav>
+        <footer class="d-none-m">
+            carlyne.lapierre@gmail.com
+        </footer>
     </header>
 </template>
 
--- src/views/app/App.vue
+++ src/views/app/App.vue
@@ -1,23 +1,12 @@
 <template>
   <div id="app">
-
     <AppSidebar></AppSidebar>
-
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
     <router-view/>
   </div>
 </template>
 
 <style lang="scss">
-@import '../../style';
-
-#app {
-  margin-left: 25%;
-}
-
+  @import './_app';
 </style>
 
 <script>
