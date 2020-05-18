4a93cab25e1a38944c65b894b18689b7e60185a5
Carlyne Lapierre <carlyne.lapierre@gmail.com>
2020-05-18 11:13:43 +0200

    initialisation projet : vue files structure, scss files strucure and start working on sidebar component (imported in app, styling in project)

---
+++ src/components/app-sidebar/AppSidebar.vue
@@ -0,0 +1,15 @@
+<template>
+    <header id="app-sidebar" class="navbar col sm-w fixed top left">
+        sidebar
+    </header>
+</template>
+
+<style lang="scss">
+    @import url('./_app-sidebar.scss');
+</style>
+
+<script>
+    export default {
+        name: 'app-sidebar'
+    }
+</script>
---
+++ src/views/app/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="app">
+
+    <AppSidebar></AppSidebar>
+
+    <div id="nav">
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link>
+    </div>
+    <router-view/>
+  </div>
+</template>
+
+<style lang="scss">
+@import '../../style';
+
+#app {
+  margin-left: 25%;
+}
+
+</style>
+
+<script>
+  import AppSidebar from '../../components/app-sidebar/AppSidebar.vue';
+
+  export default {
+    components: {
+      AppSidebar
+    }
+  }
+</script>
